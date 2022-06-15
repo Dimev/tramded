c8d343856938c5940f55bb8772ed86e41111f6f5
Dimas <skythedragon@outlook.com>
2022-06-15 20:11:57 +0200

    basic request sending, now to get it to actually work with webhooks

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,49 @@
 use clap::Parser;
+use ureq;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-	
-	/// update interval in seconds
-	#[clap(short, long, value_parser)]
-	interval: u64,
+    /// update interval in seconds
+    #[clap(short, long, value_parser)]
+    interval: u64,
 
-	/// where to send the webhook to
-	#[clap(short, long, value_parser)]
-	destination: String
+    /// where to send the webhook to
+    #[clap(short, long, value_parser)]
+    destination: String,
 }
 
 fn main() {
+    // what to do
+    let args = Args::parse();
 
-	// what to do
-	let args = Args::parse();
+    println!(
+        "Update every {} seconds, send info to '{}'",
+        args.interval, args.destination
+    );
 
-	println!("Update every {} seconds, send info to '{}'", args.interval, args.destination);
+    // say that we are online
+    if let Err(x) = ureq::post(&args.destination).send_json(ureq::json!({
+        "name": "georg"
+    })) {
+        println!("Failed to send message: {}", x);
+    }
 
-	loop {
+    loop {
+        // request
+        if let Ok(x) =
+            ureq::post("https://ovapi.nl/ovi/trains/departureboard?stopAreaId=NL%3AS%3Aut&lang=EN")
+                .send_string("")
+        {
+            // send stats if needed
+            if let Err(x) = ureq::post(&args.destination).send_json(ureq::json!({
+                "name": "georg"
+            })) {
+                println!("Failed to send message: {}", x);
+            }
+        }
 
-		// request
-		// works 90% of the time
-    	println!("Tram 22 is ded");
-
-		// wait
-		std::thread::sleep(std::time::Duration::from_secs(args.interval));
-	}
+        // wait
+        std::thread::sleep(std::time::Duration::from_secs(args.interval));
+    }
 }
